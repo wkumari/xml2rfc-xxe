074c8f91719c0d7339f0e886f783a40208a7fa46
fenner <fenner@928bd24c-ca3f-0410-b54e-cd712f733256>
2005-03-22 19:18:26 +0000

    Add a main class explaining what this jar file is, in case someone tries to run it.

--- trunk/java/xml2rfc.java
+++ trunk/java/xml2rfc.java
@@ -4,12 +4,16 @@
 //
 //  Created by Bill Fenner on Sun Nov 07 2004.
 //
+
+package com.att.research.fenner.xmleditapp.xml2rfc;
+
 import java.util.*;
 
 public class xml2rfc {
 
     public static void main (String args[]) {
-        // insert code here...
-        System.out.println("Hello World!");
+        System.out.println("This is a plugin for the XMLMind XML Editor.");
+		System.out.println("It is not meant to be invoked directly.");
+		System.out.println("See http://rtg.ietf.org/~fenner/ietf/xml2rfc-xxe/ for more info.");
     }
 }
