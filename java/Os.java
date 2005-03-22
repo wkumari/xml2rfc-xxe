b574e2c4a039c0489ba1407c49ca5e1a707d7553
fenner <fenner@users.noreply.github.com>
2005-03-22 19:44:40 +0000

    Move comments to javadoc.

--- java/Os.java
+++ java/Os.java
@@ -2,10 +2,6 @@
 //  Os.java
 //  xml2rfc
 //
-// (these should be class javadoc comments)
-//  Implement a command parallel to a process's <shell platform=> -
-//    os <platform> is executable if we're running on that platform;
-//    os with no arguments is always executable and returns the platform
 //  Created by Bill Fenner on 3/22/05.
 //  Copyright 2005 Bill Fenner. All rights reserved.
 //
@@ -16,6 +12,14 @@ import com.xmlmind.xmledit.gadget.Command;
 import com.xmlmind.xmledit.gadget.Gadget;
 import com.xmlmind.xmledit.util.*;
 
+/**
+ * Implements a Command parallel to a &ltprocess>'s &ltshell platform=""> -
+ * is executable if running on the platform passed as the parameter, or
+ * if no parameter; always returns the platform name.
+ *
+ * @param  platform Check if we're running on this platform
+ * @return          The platform name
+ */
 public class Os implements Command {
 
     public boolean prepareCommand(Gadget gadget, 
